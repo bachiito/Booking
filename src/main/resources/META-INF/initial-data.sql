ac68b1617661328dd448a1c955c6b5416e508ad2
Abraham Rivera <riveraabraham332@gmail.com>
2025-03-22 17:05:43 -0400

    Add books model

--- src/main/resources/META-INF/initial-data.sql
+++ src/main/resources/META-INF/initial-data.sql
@@ -1,3 +1,9 @@
-INSERT INTO book (name, price) VALUES ('Bible', 4); 
-INSERT INTO book (name, price) VALUES ('John', 8); 
-INSERT INTO book (name, price) VALUES ('Mark', 12); 
+INSERT INTO books (price, publication_date, author, title, genre) VALUES (22.00, '2020-01-01', 'George Orwell', '1984', 'FICITION');
+
+INSERT INTO books (price, publication_date, author, title, genre) VALUES (29.99, '2023-05-15', 'Stephen King', 'The Shining', 'THRILLER');
+
+INSERT INTO books (price, publication_date, author, title, genre) VALUES (15.50, '2022-11-20', 'Agatha Christie', 'Murder on the Orient Express', 'MYSTERY');
+    
+INSERT INTO books (price, publication_date, author, title, genre) VALUES (12.75, '2023-01-10', 'Jane Austen', 'Pride and Prejudice', 'NOVEL');
+
+INSERT INTO books (price, publication_date, author, title, genre) VALUES (18.99, '2022-08-05', 'Ernest Hemingway', 'The Old Man and the Sea', 'NARRATIVE');
