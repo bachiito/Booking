bfc9569bb16a018bd8e994b1ddc3bc4d49442c5c
Abraham Rivera <riveraabraham332@gmail.com>
2025-03-25 20:33:50 -0400

    Implement book saving functionality

--- src/main/java/com/mycompany/bookingmanager/view/BookView.java
+++ src/main/java/com/mycompany/bookingmanager/view/BookView.java
@@ -4,9 +4,12 @@ import com.mycompany.bookingmanager.entity.Book;
 import com.mycompany.bookingmanager.controller.BookController;
 import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
 import java.util.List;
+import org.primefaces.PrimeFaces;
 
 @Named
 @ApplicationScoped
@@ -17,12 +20,30 @@ public class BookView {
 
     private List<Book> books;
 
+    private Book newBook;
+
     @PostConstruct
-    public void init() {
+    public void fetchBooks() {
         books = bookService.findAll();
     }
 
     public List<Book> getBooks() {
         return books;
+    }
+
+    public Book getNewBook() {
+        return newBook;
+    }
+
+    public void clearNewBook() {
+        newBook = new Book();
+    }
+
+    public void saveBook() {
+        bookService.create(newBook);
+        fetchBooks();
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Libro guardado correctamente."));
+        PrimeFaces.current().executeScript("PF('bookCreationDialog').hide()");
+        PrimeFaces.current().ajax().update("form:alert", "form:dt-books");
     }
 }
